eb022fcb95c4972889260beb04254fce59a14160
taczhed <patryklach99@gmail.com>
2025-03-30 13:43:04 +0200

    feat(lab-03): soap exercise

--- LAB03/SoapServiceAndClient/SoapService/Program.cs
+++ LAB03/SoapServiceAndClient/SoapService/Program.cs
@@ -1,5 +1,14 @@
+using SoapCore;
+using SoapService.ServiceContract;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSoapCore();
+builder.Services.AddSingleton<IUserService, UserService>();
+
 var app = builder.Build();
+
+app.UseSoapEndpoint<IUserService>("/UserService.asmx", new SoapEncoderOptions());
 
 
 app.Run();
---
+++ LAB03/SoapServiceAndClient/SoapService/UserService.cs
@@ -0,0 +1,35 @@
+﻿using SoapService.DataContract;
+using System.ServiceModel;
+
+namespace SoapService.ServiceContract
+{
+    [ServiceContract]
+    public interface IUserService
+    {
+        [OperationContract]
+        public string RegisterUser(User user);
+    }
+
+    public class UserService : IUserService
+    {
+        public string RegisterUser(User user)
+        {
+            if (Validate(user))
+            {
+                // some registration logic
+                return $"User {user.EmailAddress} registered!";
+            }
+            return "Cannot register user.";
+        }
+
+        private bool Validate(User user)
+        {
+            if (user == null)
+                return false;
+            else if (string.IsNullOrEmpty(user.EmailAddress))
+                return false;
+            else
+                return true;
+        }
+    }
+}
